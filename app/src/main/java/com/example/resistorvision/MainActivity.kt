f9790c2c53a4a25f0dfc21a18f7b5e88c3bd4d53
m_tanaka <m_tanaka@MacBook-Pro.local>
2021-11-28 22:11:36 +0900

    first commit

---
+++ app/src/main/java/com/example/resistorvision/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.resistorvision
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.opencv.android.OpenCVLoader
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
